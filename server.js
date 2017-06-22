c73064ef36b5f24c5783dfb5f49149c53f99f389
uharband <uzi.harband@gmail.com>
2017-06-22 21:23:25 +0300

    added sharp turns

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var rpio = require('rpio');
 fs = require('fs');
 var shell = require('shelljs');
+var Enum = require('enum');
 
 var right_forward = 19;
 var left_forward = 13;
@@ -11,6 +12,106 @@ var left_backward = 15;
 
 var status = "stopped";
 
+var vertical_state = new Enum(['going_forward','going_backwards','still']);
+var horizontal_state = new Enum(['none','right_moderate','right_sharp','left_moderate','left_sharp']);
+var v_state = vertical_state.still;
+var h_state = horizontal_state.none;
+
+function rightForward(){
+	rpio.write(right_forward, rpio.HIGH);
+	rpio.write(right_backward, rpio.LOW);
+}
+function rightBackwards(){
+	rpio.write(right_backward, rpio.HIGH);
+	rpio.write(right_forward, rpio.LOW);
+}
+function leftForward(){
+	rpio.write(left_forward, rpio.HIGH);
+	rpio.write(left_backward, rpio.LOW);
+}
+function leftBackward(){
+	rpio.write(left_backward, rpio.HIGH);
+	rpio.write(left_forward, rpio.LOW);
+}
+function rightStop(){
+	rpio.write(right_backward, rpio.LOW);
+	rpio.write(right_forward, rpio.LOW);
+}
+function leftStop(){
+	rpio.write(left_backward, rpio.LOW);
+	rpio.write(left_forward, rpio.LOW);
+}
+
+function forward(){
+	rightForward();
+	leftForward();
+	v_state = vertical_state.going_forward;
+}
+
+function backwards(){
+	rightBackwards();
+	leftBackwards();
+	v_state = vertical_state.going_backwards;
+}
+
+function startModerateRight(){
+	rightStop();
+	switch(v_state){
+		case vertical_state.still:
+			leftForward();
+			break;
+	}
+}
+
+function startModerateLeft(){
+	leftStop();
+	switch(v_state){
+		case vertical_state.still:
+			rightForward();
+			break;
+	}
+}
+
+function startSharpRight(){
+	switch(v_state){
+		case vertical_state.going_forward:
+			rightBackward();
+			leftForward();
+			break;
+		case vertical_state.going_backward:
+			rightForward();
+			leftBackward();
+			break;			
+	}
+}
+
+function startSharpLeft(){
+	switch(v_state){
+		case vertical_state.going_forward:
+			leftBackward();
+			rightForward();
+			break;
+		case vertical_state.going_backward:
+			leftForward();
+			rightBackward();
+			break;			
+	}
+}
+
+
+
+function stopTurning(){
+	switch(v_state){
+		case vertical_state.going_forward:
+			forward();
+			break;
+		case vertical_state.going_backwads:
+			backward();
+			break;
+		case vertical_state.still:
+			stop();
+	}		
+}
 
 app.use(express.static(__dirname));
 
@@ -78,22 +179,41 @@ app.get('/audio/off', function (req, res) {
 // car controller
 
 app.get('/forward', function (req, res) {
-    stop();    
-    rpio.write(right_forward, rpio.HIGH);
-    rpio.write(left_forward, rpio.HIGH);
-    status = "forward";
-    res.send('forward');
+    forward();
+	v_state = vertical_status.going_forward;
+    res.send('going forward');
 })
 
 app.get('/backward', function (req, res) {
-    stop();
-    rpio.write(left_backward, rpio.HIGH);
-    rpio.write(right_backward, rpio.HIGH);
-    status = "backward"
-    res.send('backward');
+    backwards();
+	v_state = vertical_status.going_backwards;
+    res.send('going backwards');
 })
 
+app.get('/moderateRight', function (req, res) {
+    startModerateRight();
+    res.send('moderate right');
+})
 
+app.get('/moderateLeft', function (req, res) {
+    startModerateLeft();
+    res.send('moderate left');
+})
+
+app.get('/sharpRight', function (req, res) {
+    startSharpRight();
+    res.send('sharp right');
+})
+
+app.get('/sharpLeft', function (req, res) {
+    startSharpLeft();
+    res.send('sharp left');
+})
+
+app.get('/stopTurning', function (req, res) {
+    stopTurning();
+    res.send('stopped turning');
+})
 
 app.get('/left/:degrees', function (req, res) {    
     var deg = req.params['degrees'];
@@ -124,6 +244,7 @@ app.get('/left/:degrees', function (req, res) {
     
     res.send('turned left ' + deg);
 })
+
 
 app.get('/right/:degrees', function (req, res) {
     var deg = req.params['degrees'];
@@ -156,6 +277,8 @@ app.get('/right/:degrees', function (req, res) {
     
     
 })
+
+
 
 
 
