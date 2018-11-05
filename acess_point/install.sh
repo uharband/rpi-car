5710b95a8e9f1eb1569ad3f1b50251229909ce66
uharband <uzi.harband@gmail.com>
2018-11-05 06:15:16 +0200

    removed old non working output to ipv4

--- acess_point/install.sh
+++ acess_point/install.sh
@@ -106,7 +106,6 @@ sudo mv $x /etc/default/hostapd
 
 touch $x
 sudo /bin/su -c "echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf"
-sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
