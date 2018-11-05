74fb8e4d1cbffd5a3a0a8ad0cae489d775921f03
uharband <uzi.harband@gmail.com>
2018-11-05 06:17:32 +0200

    add audio

---
+++ audio/install.sh
@@ -0,0 +1,6 @@
+# install
+
+# ffmpeg is part of raspbian in the 'streach' release
+sudo apt-get install vlc -y
+sudo apt-get install screen
+
---
+++ audio/readme.md
@@ -0,0 +1,5 @@
+to install mjpg_steamer
+
+1. run: chmod +x install.sh
+2. run: ./install
+
