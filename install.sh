8be2f5b4909e618e29e9fc43bd57c60c6a12fc1b
uharband <uzi.harband@gmail.com>
2019-01-13 21:10:00 +0200

    removed installing ap and rc.local

--- install.sh
+++ install.sh
@@ -7,20 +7,19 @@ echo "  ----------------------------------------------------------- "
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 echo current directory is `pwd`
-./access_point/install.sh
+#./access_point/install.sh
 ./server/install.sh
-
 ./audio/install.sh
 ./video/install.sh
 
-sudo sed -i -e '/exit 0/{r rc.local/add-to-rc.local.txt' -e 'd' -e '}' /etc/rc.local
+#sudo sed -i -e '/exit 0/{r rc.local/add-to-rc.local.txt' -e 'd' -e '}' /etc/rc.local
 
-echo "  --------  after updating /etc/rc.local, the content of rc.local is:  ---------- "
+#echo "  --------  after updating /etc/rc.local, the content of rc.local is:  ---------- "
 
 cat /etc/rc.local
 
 echo "  ----------------------------------------------------------- "
 echo ""
-echo "  --------------  starting installing   --------------------- "
+echo "  --------------  finished installing   --------------------- "
 echo ""
 echo "  ----------------------------------------------------------- "
