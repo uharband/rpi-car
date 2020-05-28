b1d7a1fcb5330b1a42426e761c82de47b8145192
uharband <uzi.harband@gmail.com>
2020-05-28 10:09:15 +0300

    end response

--- server/lib/car/carSimulatorRouter.js
+++ server/lib/car/carSimulatorRouter.js
@@ -5,22 +5,22 @@ let carSimulator = require("./carSimulator");
 
 carSimulatorRouter.get('/init-motor', function (req, res) {
     carSimulator.initMotor(req.query.motor, req.query.mode, req.query.pin1, req.query.pin2);
-    res.send();
+    res.end();
 });
 
 carSimulatorRouter.get('/forward', function (req, res) {
     carSimulator.forward(req.query.motor, req.query.speed);
-    res.send();
+    res.end();
 });
 
 carSimulatorRouter.get('/backwards', function (req, res) {
     carSimulator.backwards(req.query.motor, req.query.speed);
-    res.send();
+    res.end();
 });
 
 carSimulatorRouter.get('/forward', function (req, res) {
     carSimulator.stop(req.query.motor, req.query.speed);
-    res.send();
+    res.end();
 });
 
 module.exports = carSimulatorRouter;
